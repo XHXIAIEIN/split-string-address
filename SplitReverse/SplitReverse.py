af9f664b8d84a5f0adc3b55f4ae31686c6bd88ac
XHXIAIEIN <45864744+XHXIAIEIN@users.noreply.github.com>
2020-09-28 16:04:28 +0800

    Update SplitReverse.py

--- SplitReverse/SplitReverse.py
+++ SplitReverse/SplitReverse.py
@@ -12,25 +12,30 @@ class SplitReverseCommand(sublime_plugin.TextCommand):
         # 临时变量
         str_name, str_phone, str_address = '', '', ''
 
-        #去除多余字符
-        text = text.strip().lstrip().rstrip().replace('\n', ' ')
-        regex  = r"(\+)|(\\)|(\.)|(\~)|(，)|(。)|(,)|(;)|(：)|(:)|(？)|(收件地址)|(收货地址)|(所在地区)|(详细地址)|(联系号码)|(手机号码)|(联系电话)|(手机号)|(收货人)|(收件人)|(（收）)|(号码)|(手机)|(名字)|(地址)|(电话)|(姓名)|(可能是电话号码，是否拨号?)|(在地图中查看)|(\?)"
-        text = re.sub(regex ,' ',text) 
+        # 正则匹配规则
+        regex_phone   =  r'1\d{10}'
+        regex_phone2  =  r'(\d{3})(-)?(\d{4})(-)?(\d{4})'
+        regex_address =  r'([^(\d|\s)$省]+省|.+自治区)?([^市]+市)([^县]+县|.+区)?(.*)'
+        regex_split   =  r'(\n)|(\+)|(\\)|(\.)|(\~)|(，)|(。)|(,)|(;)|(？)|(：)|(:)|(\?)'
+        regex_delete  =  r'(收件地址)|(收货地址)|(所在地区)|(详细地址)|(联系号码)|(手机号码)|(联系电话)|(手机号)|(收货人)|(收件人)|(（收）)|(号码)|(手机)|(名字)|(地址)|(电话)|(姓名)|(可能是电话号码，是否拨号?)|(在地图中查看)'
 
-        # 分隔空格
-        group = text.split()
+        # 分隔换行符
+        split_line = re.sub(regex_split ,' ', text.strip().lstrip().rstrip()).split()
 
-        print(group)
+        # print(split_line)
 
         # 提取信息
-        for s in group:
-            if s == '': continue
-            elif re.findall(r"(.*?)(省|自治区|市)|(.*?)(区|县|州)|(.*?)(街道|大道|巷|路口)|(.*?)(大厦|小区|工作室|公园|花园|公司|公寓|宿舍|驿站|学府|超市|旅馆|百货|便利店|大学)|(.*?)(室|座|号|楼|栋|单元)", s):  str_address = str_address + s
-            elif re.findall(r'1\d{10}', s): str_phone = str_phone + s
-            else: str_name = str_name +s
-        
+        for s in split_line: 
+            s = s.replace(' ', '')
+            if s == '': continue;
+            elif re.findall(regex_phone , s):  str_phone = str_phone + s;
+            elif re.findall(regex_phone2 , s): s = s.replace('-' , ''); str_phone = str_phone + s;
+            elif re.findall(regex_address, s): str_address = str_address + s;
+            else: str_name = str_name +s;
+
         # 组合信息
         str_text = ' '.join([str_name, str_phone, str_address])
+        str_text = re.sub(regex_delete ,'', str_text)
 
         # 替换文本
         self.view.replace(edit, sel, str_text)
